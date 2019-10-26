3e3367e11fcb1adced6d8ba29f85da21f3d3ddae
dheatara <56398506+dheatara@users.noreply.github.com>
2019-10-26 12:34:39 +0700

    Add files via upload

---
+++ Tugas 1.py
@@ -0,0 +1,14 @@
+namalengkap = "Dhea Tara Monika"
+namapanggilan = "Dhea"
+npm = "3119010611"
+tempatlahir = "Tegal"
+umur = "19"
+telp = "0823-2829-9797"
+alamat = "Jayamukti Cikarang Pusat"
+
+print("Assalamualaikum,\n"
+"let me introduce my self, my name is",namalengkap,
+"but you can call me",namapanggilan,
+", my npm is",npm,
+", i was bron in",tempatlahir,
+"and i am",umur,"years old. i am very glad if you want to invite my house in",alamat,",so dont forget to call me before with the number",telp,"\ thank you")
